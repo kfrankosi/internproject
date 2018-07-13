e9d289089960928256bdb3e7c24a096f704a8cc6
Kelli Frank <kfrank@osisoft.com>
2018-07-13 13:39:13 -0700

    loading w map centerd at osi but no marker

--- InternProject/Scripts/app.js
+++ InternProject/Scripts/app.js
@@ -130,10 +130,10 @@ function renderMap() {
         if (status === 'OK') {
             console.log(results[0]);
             map.setCenter(results[0].geometry.location);
-            var marker = new google.maps.Marker({
-                map: map,
-                position: results[0].geometry.location
-            });
+            // var marker = new google.maps.Marker({
+            //     map: map,
+            //     position: results[0].geometry.location
+            // });
         } else {
             alert('Geocode was not successful for the following reason: ' + status);
         }
